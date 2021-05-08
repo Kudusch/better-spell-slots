538e28507701643722f72f29c4ec6a14c9723172
Kudusch <kudusch@posteo.de>
2021-05-09 01:17:01 +0200

    Add visual spell slot trackers

--- better-spell-slots.js
+++ better-spell-slots.js
@@ -1,8 +1,34 @@
 const MODULE_NAME = "better-spell-slots";
 
+function gen_spellslots(slots, uses) {
+    var i;
+    var used_slot = "<div class='spellslot'><div class='spellslot-content'></div></div>";
+    var unused_slot = "<div class='spellslot spellslot-available'><div class='spellslot-content'></div></div>";
+    var out_html = [];
+    for (i = 1; i <= slots; i++) {
+        if (i > uses) {
+            out_html.push(used_slot);
+        } else {
+            out_html.push(unused_slot);
+        }
+    }
+    //out_html.reverse();
+    return("<div class='slots'>" + out_html.join("\n") + "</div>");
+}
+
 Hooks.on("renderTidy5eSheet", (app, html, data) => {
-    console.log(data.actor);
-    console.log(app);
-    console.log(html);
-    console.log("Tidy5e Sheet rendered!");
+    console.log("<---- better-spell-slots");
+    var spellbok_data = data.actor.data.spells;
+    var spellbook_sections = $( document ).find(".spell-detail.spell-slots");
+    var spell_level = 1;
+    spellbook_sections.each(function(){
+        
+        var slots = spellbok_data["spell"+spell_level]["max"];
+        var uses =  slots - spellbok_data["spell"+spell_level]["value"];
+        var slots_html = gen_spellslots(slots, uses);
+        $(this).append(slots_html);
+        spell_level++;
+    })
+    
+    console.log("<---- better-spell-slots");
 });
